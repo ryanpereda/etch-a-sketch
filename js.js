0c063819efdca4093f6fee509e6713dc423fbfc7
ryanpereda <ryanpereda9001@gmail.com>
2025-10-17 04:49:35 -0400

    added reset and resize button

--- js.js
+++ js.js
@@ -1,16 +1,44 @@
 let container = document.querySelector(".container")
+let resetButton = document.querySelector(".reset")
+let resizeButton = document.querySelector(".resize")
+let DEFAULT = 16
+let previous = DEFAULT
 
-for (let i = 0; i < 16; i++) {
-    let divRow = document.createElement("div")
-    divRow.className = "divrow"
-    for (let j = 0; j < 16; j++) {
-        let divSquare = document.createElement("div")
-        divSquare.className = "divsquare"
-        divSquare.textContent = i
-        divRow.appendChild(divSquare)
-        divSquare.addEventListener('mouseenter', () => {
-            divSquare.style.cssText = 'background-color: black;'
-        })
+function generateGrid(size) {
+    previous = size
+    for (let i = 0; i < size; i++) {
+        let divRow = document.createElement("div")
+        divRow.className = "divrow"
+        for (let j = 0; j < size; j++) {
+            let divSquare = document.createElement("div")
+            divSquare.className = "divsquare"
+            divRow.appendChild(divSquare)
+            divSquare.addEventListener('mouseenter', () => {
+                divSquare.style.cssText = 'background-color: black;'
+            })
+        }
+        container.appendChild(divRow)
     }
-    container.appendChild(divRow)
 }
+
+function clearGrid() {
+    container.replaceChildren()
+}
+
+resetButton.onclick = () => {
+    clearGrid()
+    generateGrid(previous)
+}
+
+resizeButton.onclick = () => {
+    let number = parseInt(prompt("Enter a number between 1 and 100:", "16"));
+    if (0 < number && number < 101) {
+        clearGrid()
+        generateGrid(number)
+    } else {
+        alert("Input not between 1 and 100.")
+    }
+}
+
+generateGrid(DEFAULT)
+
